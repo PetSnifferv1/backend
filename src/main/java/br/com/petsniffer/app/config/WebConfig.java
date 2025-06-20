5ceb794465a1f570e034d7dda7c40282d40cd7ed
Fabio Tiroli <fabiolobotiroli@gmail.com>
2025-06-20 12:26:40 -0300

    correção Cors webmvc

--- src/main/java/br/com/petsniffer/app/config/WebConfig.java
+++ src/main/java/br/com/petsniffer/app/config/WebConfig.java
@@ -3,9 +3,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://petsniffer.com.br:5173",
+                        "http://petsniffer.com.br",
+                        "http://www.petsniffer.com.br",
+                        "http://www.petsniffer.com.br:5173",
+                        "http://petsniffer-alb-298396905.us-east-1.elb.amazonaws.com",
+                        "http://petsniffer-alb-298396905.us-east-1.elb.amazonaws.com:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
     @Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
