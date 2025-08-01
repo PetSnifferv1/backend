81946204b3fad8916e5c5ceaaf3aa3e04e7b8223
Fabio Tiroli <fabiolobotiroli@gmail.com>
2025-08-01 03:08:51 -0300

    incluindo chat

--- src/main/java/br/com/petsniffer/app/config/AWSConfig.java
+++ src/main/java/br/com/petsniffer/app/config/AWSConfig.java
@@ -1,32 +1,31 @@
 package br.com.petsniffer.app.config;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.context.annotation.Configuration;
 
+    @Getter
     @Configuration
     public class AWSConfig {
         private final AmazonS3 s3Client;
 
-         public AWSConfig() {
-            // Caso esteja usando credenciais fixas
-            BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIASK5MCDTOOXLAURJ2", "TN3u95Md62u+edbNNaj04jzfXwaXAnoqlTwG3oKW");
+        public AWSConfig(@Value("${AWSACCESKEY}") String accessKey,
+                        @Value("${AWSSECRETKEY}") String secretKey,
+                        @Value("${AWS_REGION}") String region) {
+            BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+
             this.s3Client = AmazonS3ClientBuilder.standard()
                     .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                    .withRegion("us-east-1") // Substitua pela sua região
+                    .withRegion(region)
                     .build();
         }
 
-        public AmazonS3 getS3Client() {
-            return s3Client;
-        }
 
-
-
-
-
- }
+    }
 
 
