156583618271ce66faff8f9fcd8db99b40d48775
Fabio Tiroli <fabiolobotiroli@gmail.com>
2025-09-07 09:40:25 -0300

    resolver problema aws config em testes

--- src/main/java/br/com/petsniffer/app/config/AWSConfig.java
+++ src/main/java/br/com/petsniffer/app/config/AWSConfig.java
@@ -8,9 +8,11 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Profile;
 
-    @Getter
+@Getter
     @Configuration
+    @Profile("!test")
     public class AWSConfig {
         private final AmazonS3 s3Client;
 
