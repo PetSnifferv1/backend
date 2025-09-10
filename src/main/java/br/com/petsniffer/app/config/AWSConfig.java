e6b17183e57a1e2cf8f4471e7b79b032ee0d83ae
Fabio Tiroli <fabiolobotiroli@gmail.com>
2025-09-10 14:19:35 -0300

    coocando secrets aws no actions

--- src/main/java/br/com/petsniffer/app/config/AWSConfig.java
+++ src/main/java/br/com/petsniffer/app/config/AWSConfig.java
@@ -2,6 +2,7 @@ package br.com.petsniffer.app.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -14,19 +15,38 @@ import org.springframework.context.annotation.Profile;
     @Configuration
     @Profile("!test")
     public class AWSConfig {
-        private final AmazonS3 s3Client;
 
 
-        public AWSConfig(@Value("${AWS_ACCESS_KEY_ID}") String accessKey,
-                        @Value("${AWS_SECRET_ACCESS_KEY}") String secretKey,
-                        @Value("${AWS_REGION}") String region) {
-            BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+    // Injeta a região a partir das propriedades. Isso é seguro e útil.
+    @Value("${AWS_REGION}")
+    private String region;
 
-            this.s3Client = AmazonS3ClientBuilder.standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                    .withRegion(region)
-                    .build();
-        }
+    @Bean
+    public AmazonS3 s3Client() {
+        // O SDK da AWS usará o "DefaultAWSCredentialsProviderChain".
+        // Em produção (ECS), ele encontrará a IAM Role da tarefa.
+        // Em desenvolvimento local, ele pode encontrar variáveis de ambiente
+        // ou o arquivo ~/.aws/credentials.
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .build();
+    }
+
+//=================================================================================================\\
+//        private final AmazonS3 s3Client;
+
+
+
+    //      public AWSConfig(@Value("${AWS_ACCESS_KEY_ID}") String accessKey,
+//                        @Value("${AWS_SECRET_ACCESS_KEY}") String secretKey,
+//                        @Value("${AWS_REGION}") String region) {
+//            BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+//
+//            this.s3Client = AmazonS3ClientBuilder.standard()
+//                    .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+//                    .withRegion(region)
+//                    .build();
+//        }
 
 
     }
